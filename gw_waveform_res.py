0956f03ebd0dc316c8574e9cde4e04539f8e5a55
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-08-29 11:52:44 -0500

    hyp_pta_res function

--- gw_waveform_res.py
+++ gw_waveform_res.py
@@ -12,7 +12,9 @@ from rr_method2 import solve_rr2
 from hypmik3pn import get_u, get_u_v2
 from scipy.integrate import cumtrapz
 from scipy.optimize import curve_fit
-
+from astropy.cosmology import Planck18
+from scipy.interpolate import CubicSpline
+from enterprise.signals import signal_base
 
 def get_hyp_waveform(M,q,et0,b,ti,tf,t_step,inc,distance,order
                      ,estimatepeak='None',rr='None',rrmethod='None'):
@@ -92,11 +94,81 @@ def get_hyp_waveform(M,q,et0,b,ti,tf,t_step,inc,distance,order
             return Hp-Hp[0],Hx-Hx[0]
 
 
-
-
 def func(x, a0, a1, a2):
     return (a0+a1*x+a2*x**2)
 
+
+@signal_base.function
+def hyp_pta_res(toas,
+    theta,
+    phi,
+    cos_gwtheta,
+    gwphi,
+    psi,
+    cos_inc,
+    log10_M,
+    q,
+    b,
+    e0,
+    log10_z,
+    interp_steps=1000
+):
+    """
+    Compute the PTA signal due to a hyperbolic encounter.
+
+    toas        are pulsar toas in s in SSB frame
+    theta       is pulsar zenith angle in rad
+    phi         is pulsar RA in rad
+    cos_gwtheta is cos zenith angle of the GW source
+    gwphi       is the RA of the GW source in rad
+    psi         is the GW polarization angle in rad
+    cos_inc     is the cos inclination of the GW source
+    log10_M     is the log10 total mass of the GW source in solar mass
+    q           is the mass ratio of the GW source
+    b           is the impact parameter of the GW source in solar mass
+    e0          is the eccentricity of the GW source
+    log10_z     is the log10 cosmological redshift of the GW source
+    interp_steps is the number of samples used for interpolating the PTA signal
+    """
+    order = 3
+
+    z = 10**log10_z
+    D_GW = 1e6 * Planck18.luminosity_distance(z).value * pc # meter
+
+    # ti, tf, tzs in seconds, in source frame
+    ti = min(toas)/(1+z)
+    tf = max(toas)/(1+z)
+    tz_arr = np.linspace(ti, tf, interp_steps)
+    delta_t_arr = (tz_arr[1]-tz_arr[0]) * (1+z) # second, in SSB frame
+
+    tzs = toas/(1+z)
+    
+    M = 10**log10_M # Solar mass
+
+    inc = np.arccos(cos_inc)
+
+    gwra = gwphi
+    gwdec = np.arcsin(cos_gwtheta)
+
+    psrra = phi
+    psrdec = np.pi/2 - theta
+
+    hp_arr, hx_arr = get_hyp_waveform(M, q, e0, b, ti, tf, interp_steps, inc, D_GW, order)
+
+    cosmu, Fp, Fx = ap.antenna_pattern(gwra, gwdec, psrra, psrdec)
+
+    c2psi = np.cos(2*psi)
+    s2psi = np.sin(2*psi)
+    Rpsi = np.array([[c2psi, -s2psi],
+                     [s2psi, c2psi]])
+    h_arr = np.dot([Fp,Fx], np.dot(Rpsi, [hp_arr,hx_arr]))
+
+    # Integrate over time in SSB frame
+    s_arr = cumtrapz(h_arr, initial=0)*delta_t_arr
+    s_spline = CubicSpline(tz_arr, s_arr)
+    s = s_spline(tzs)
+
+    return s
 
 class waveform:
    
